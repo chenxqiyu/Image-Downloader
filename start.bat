b438fb5e173c405bb839f8d4b512fe28cb70b96c
chenxqiyu <390739790@qq.com>
2022-04-25 11:08:43 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,5 @@
+@echo off
+python -m pip install -i http://mirrors.aliyun.com/pypi/simple --upgrade pip
+python -m pip install -i http://mirrors.aliyun.com/pypi/simple -r requirements.txt
 python image_downloader_gui.py
 pause
